65e68b1ded2d88980dc1325a5906ea656426320e
sp4ghet <8775460+sp4ghet@users.noreply.github.com>
2019-11-08 01:16:45 +0000

    Break program into smaller modules for each struct

---
+++ src/user_input.rs
@@ -0,0 +1,34 @@
+use winit::{Event, EventsLoop, WindowEvent};
+
+#[derive(Debug, Clone, Default)]
+pub struct UserInput {
+    pub end_requested: bool,
+    pub new_frame_size: Option<(f64, f64)>,
+    pub new_mouse_position: Option<(f64, f64)>,
+}
+
+impl UserInput {
+    pub fn poll_events(events_loop: &mut EventsLoop) -> Self {
+        let mut output = UserInput::default();
+        events_loop.poll_events(|event| match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => output.end_requested = true,
+            Event::WindowEvent {
+                event: WindowEvent::CursorMoved { position, .. },
+                ..
+            } => {
+                output.new_mouse_position = Some((position.x, position.y));
+            }
+            Event::WindowEvent {
+                event: WindowEvent::Resized(logical),
+                ..
+            } => {
+                output.new_frame_size = Some((logical.width, logical.height));
+            }
+            _ => (),
+        });
+        output
+    }
+}
